3e0454fdedbd96218d43d913c5a175a8f9e1b2aa
haram8009 <84117653+haram8009@users.noreply.github.com>
2024-02-15 02:06:37 +0900

    [level 1] Title: 덧칠하기, Time: 1.70 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,12 +1,11 @@
 def solution(n, m, section):
-    answer = 0
-    section = set(section)
-
-    i = 1
-    while(i <= n):
-        if i in section:
+    answer = 1
+    
+    prev = section[0]
+    
+    for sec in section:
+        if sec - prev >= m:
             answer+=1
-            i+=m
-        else:
-            i+=1
+            prev = sec
+    
     return answer
