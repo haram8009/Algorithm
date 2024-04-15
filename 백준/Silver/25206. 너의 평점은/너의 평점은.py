fa0e6cb64ba31b19be6e74425a520531cf45669c
haram8009 <84117653+haram8009@users.noreply.github.com>
2024-04-15 18:55:56 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,22 @@
+grades = {"A+": 4.5, "A0":	4.0,
+"B+":	3.5,
+"B0":	3.0,
+"C+":	2.5,
+"C0":	2.0,
+"D+":	1.5,
+"D0":   1.0,
+"F":	0.0
+        }
+
+total = 0.0
+cnt = 0.0
+for i in range(20):
+    name, credit, grade = input().split()
+    
+    if grade != "P":
+        total += float(credit) * grades[grade]
+        cnt += float(credit)
+
+print(total/cnt)    
+
+    
