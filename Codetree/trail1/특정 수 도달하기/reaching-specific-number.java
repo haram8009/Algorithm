b789bd586723639f16d5020fe6d6fabdf82f5100
Haram Lee <84117653+haram8009@users.noreply.github.com>
2026-07-03 03:54:02 +0900

    [Passed] 특정 수 도달하기 (132ms, 10MB)

---
+++ Codetree/trail1/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        for (int i=0; i<10; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int sum=0;
+        int cnt=0;
+        for(int n:arr){
+            if (n>=250){
+                break;
+            }
+            sum+=n;
+            cnt++;
+        }
+
+        System.out.print(sum+" ");
+        System.out.print(Math.round(((double)sum/cnt)*10)/10.0);
+    }
+}
