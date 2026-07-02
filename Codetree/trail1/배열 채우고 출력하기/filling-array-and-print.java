034042fd2082adb15e8ce99fe6785b307d146cc5
Haram Lee <84117653+haram8009@users.noreply.github.com>
2026-07-03 03:35:00 +0900

    [Passed] 배열 채우고 출력하기 (129ms, 10MB)

---
+++ Codetree/trail1/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+        for(int i=9; i>=0; i--){
+            System.out.print(arr[i]);
+        }
+    }
+}
