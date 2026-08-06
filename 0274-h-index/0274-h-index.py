5135cb51b4fc37efcb60f4f48768bdb6aa5af49b
Haram Lee <84117653+haram8009@users.noreply.github.com>
2026-08-06 22:47:41 +0900

    Time: 207 ms (5.04%), Space: 19.4 MB (25.37%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,14 +4,13 @@ class Solution:
         n=len(citations)
 
         while(True):
+            if h-1 > n:
+                return n
             if -1 in citations:
                 if (n-citations.count(-1)) < h:
                     break
-            # print("b", h, citations)
             h += 1
             for i in range(n):
                 if citations[i] > -1:
                     citations[i] -= 1
-            # print("d", h, citations)
-        return h - 1 if h-1<=n else n
-    
+        return h - 1 
