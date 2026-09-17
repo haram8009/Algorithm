50212293c1325165ba0d18c88aa43304dca3c66d
Haram Lee <84117653+haram8009@users.noreply.github.com>
2026-09-17 09:53:52 +0900

    [Passed] 자연수 M/2개의 쌍 (1697ms, 75MB)

--- Codetree/trail5/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ Codetree/trail5/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -2,44 +2,34 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static class Node {
-        int num;
-        int cnt;
-
-        public Node(int num, int cnt) {
-            this.num = num;
-            this.cnt = cnt;
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Node[] arr = new Node[n];
+        int[][] arr = new int[n][];
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            arr[i] = new Node(y, x);
+            arr[i] = new int[] { x, y };
         }
 
         Arrays.sort(arr, (a, b) -> {
-            return a.num - b.num;
+            return a[1] - b[1];
         });
 
         int answer = 0;
 
         int i = 0, j = n - 1, c;
         while (i <= j && i < n && j >= 0) {
-            Node n1 = arr[i];
-            Node n2 = arr[j];
-            c = n1.num + n2.num;
-            int sub = Math.abs(n1.cnt - n2.cnt);
+            int[] n1 = arr[i];
+            int[] n2 = arr[j];
+            c = n1[1] + n2[1];
+            int sub = Math.abs(n1[0] - n2[0]);
 
-            if (n1.cnt > n2.cnt) {
-                n1.cnt = sub;
+            if (n1[0] > n2[0]) {
+                n1[0] = sub;
                 j--;
-            } else if (n1.cnt < n2.cnt) {
-                n2.cnt = sub;
+            } else if (n1[0] < n2[0]) {
+                n2[0] = sub;
                 i++;
             } else {
                 i++;
@@ -48,12 +38,7 @@ public class Main {
 
             answer = c > answer ? c : answer;
         }
-//        if (i == j) {
-//            if (arr[i].cnt > 1) {
-//                answer = (arr[i].num * 2) > answer ? (arr[i].num * 2) : answer;
-//
-//            }
-//        }
+
         System.out.println(answer);
     }
 }
